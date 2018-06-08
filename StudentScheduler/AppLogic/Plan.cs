c9495bf6c1661bc3abb785034f9593707243425a
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2018-06-08 21:07:41 +0200

    Added Flow Altering for debugging reasons
    Added flow debugging" it erases graph generated from teacher/students and builds custom one.

--- StudentScheduler/AppLogic/Plan.cs
+++ StudentScheduler/AppLogic/Plan.cs
@@ -504,6 +504,39 @@ namespace StudentScheduler.AppLogic
         private void DoItUsingFlows()
         {
             Flow flow = new Flow(teachers[0], students);
+            // Alter flow
+
+            /*flow.DEBUG_ClearNodes();
+            Node root = new Node("Input", -1, Node.NodeType.Input);
+            Node sink = new Node("Output", -1, Node.NodeType.Output);
+            // Create students 1 and 2
+            Node s1 = new Node("Student 1", -1, Node.NodeType.Default);
+            Node s2 = new Node("Student 2", -1, Node.NodeType.Default);
+            // Create times (not overlapping)
+            Node t1 = new Node("Time (100)", 100, Node.NodeType.Default);
+            Node t2 = new Node("Time (700)", 700, Node.NodeType.Default);
+            // Add time chunk
+            Node tch = new Node("Time Chunk Node", -1, Node.NodeType.Default);
+            // Add paths from root to students and from times to timechunk and from timechunk via [timechunk] to sink
+            root.OutputEdges.Add(new Edge(1, 0, root, s1));
+            root.OutputEdges.Add(new Edge(1, 0, root, s2));
+            s1.InputEdges.Add(root.OutputEdges[0]);
+            s2.InputEdges.Add(root.OutputEdges[1]);
+
+            t1.OutputEdges.Add(new Edge(1, 0, t1, tch));
+            t2.OutputEdges.Add(new Edge(1, 0, t2, tch));
+            tch.InputEdges.Add(t1.OutputEdges[0]);
+            tch.InputEdges.Add(t1.OutputEdges[0]);
+
+            tch.OutputEdges.Add(new TimeChunk(tch, sink));
+            sink.InputEdges.Add(tch.OutputEdges[0]);
+            // Add paths from students to times
+            s1.OutputEdges.Add(new Edge(1, 0, s1, t1));
+            s1.OutputEdges.Add(new Edge(1, 0, s1, t2));
+
+            // Apply new graph
+            flow.Nodes.AddRange(new List<Node>() { root, s1, s2, t1, t2, tch, sink });*/
+            // End of alter flow
             int[] breaks = flow.GetResult();
             breakAfterLessonsStart = breaks;
         }
