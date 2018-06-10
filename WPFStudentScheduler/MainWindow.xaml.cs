33cccd86a6e9ec5cf852ed244e6ac49f2ae0a122
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2018-06-10 14:35:58 +0200

    Added Flow In Network files

--- WPFStudentScheduler/MainWindow.xaml.cs
+++ WPFStudentScheduler/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentScheduler.AppLogic.NetworkFlow;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,7 @@ namespace WPFStudentScheduler
     public partial class MainWindow : Window
     {
         private StackPanel[] daysPanels;
+        private Flow flow;
 
         public MainWindow()
         {
