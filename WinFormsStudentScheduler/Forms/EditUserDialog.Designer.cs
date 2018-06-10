22db5234e755d57b3c33ef806a42ff73553ead14
SoptikHa2 <SoptikHa2@users.noreply.github.com>
2018-06-10 17:59:15 +0200

    Designed Edit User Dialog

---
+++ WinFormsStudentScheduler/Forms/EditUserDialog.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WinFormsStudentScheduler.Forms
+{
+    partial class EditUserDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTextInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.InputPO = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.InputUT = new System.Windows.Forms.TextBox();
+            this.InputST = new System.Windows.Forms.TextBox();
+            this.InputCT = new System.Windows.Forms.TextBox();
+            this.InputPA = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jméno";
+            // 
+            // NameTextInput
+            // 
+            this.NameTextInput.Location = new System.Drawing.Point(67, 8);
+            this.NameTextInput.Name = "NameTextInput";
+            this.NameTextInput.Size = new System.Drawing.Size(162, 20);
+            this.NameTextInput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pondělí";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Úterý";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Středa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(12, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Čtvrtek";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(12, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Pátek";
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(12, 238);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(85, 23);
+            this.OKButton.TabIndex = 7;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Location = new System.Drawing.Point(144, 238);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(85, 23);
+            this.CancelButton.TabIndex = 8;
+            this.CancelButton.Text = "Zrušit";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // InputPO
+            // 
+            this.InputPO.Location = new System.Drawing.Point(72, 45);
+            this.InputPO.Name = "InputPO";
+            this.InputPO.Size = new System.Drawing.Size(157, 20);
+            this.InputPO.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(73, 189);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "8:00 - 16:00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(12, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Příklad";
+            // 
+            // InputUT
+            // 
+            this.InputUT.Location = new System.Drawing.Point(73, 74);
+            this.InputUT.Name = "InputUT";
+            this.InputUT.Size = new System.Drawing.Size(157, 20);
+            this.InputUT.TabIndex = 12;
+            // 
+            // InputST
+            // 
+            this.InputST.Location = new System.Drawing.Point(73, 100);
+            this.InputST.Name = "InputST";
+            this.InputST.Size = new System.Drawing.Size(157, 20);
+            this.InputST.TabIndex = 13;
+            // 
+            // InputCT
+            // 
+            this.InputCT.Location = new System.Drawing.Point(73, 126);
+            this.InputCT.Name = "InputCT";
+            this.InputCT.Size = new System.Drawing.Size(157, 20);
+            this.InputCT.TabIndex = 14;
+            // 
+            // InputPA
+            // 
+            this.InputPA.Location = new System.Drawing.Point(73, 152);
+            this.InputPA.Name = "InputPA";
+            this.InputPA.Size = new System.Drawing.Size(157, 20);
+            this.InputPA.TabIndex = 15;
+            // 
+            // EditUserDialog
+            // 
+            this.AcceptButton = this.OKButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelButton;
+            this.ClientSize = new System.Drawing.Size(242, 273);
+            this.Controls.Add(this.InputPA);
+            this.Controls.Add(this.InputCT);
+            this.Controls.Add(this.InputST);
+            this.Controls.Add(this.InputUT);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.InputPO);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameTextInput);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EditUserDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Detail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameTextInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TextBox InputPO;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox InputUT;
+        private System.Windows.Forms.TextBox InputST;
+        private System.Windows.Forms.TextBox InputCT;
+        private System.Windows.Forms.TextBox InputPA;
+    }
+}
--- WinFormsStudentScheduler/Forms/MainWindow.Designer.cs
+++ WinFormsStudentScheduler/Forms/MainWindow.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UsersListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UsersListBox
+            // 
+            this.UsersListBox.FormattingEnabled = true;
+            this.UsersListBox.Location = new System.Drawing.Point(12, 12);
+            this.UsersListBox.Name = "UsersListBox";
+            this.UsersListBox.Size = new System.Drawing.Size(120, 368);
+            this.UsersListBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Nový";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Upravit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(139, 386);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(169, 23);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Spustit program";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainWindow";
+            this.ClientSize = new System.Drawing.Size(320, 411);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UsersListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.Text = "Návrhář rozvrhu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox UsersListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button StartButton;
     }
 }
